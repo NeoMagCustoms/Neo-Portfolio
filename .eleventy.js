85637cafe0138e881b7af61993015ac2232ba88d
Magnetar StarHeart <krysssativa@gmail.com>
2025-09-16 15:29:02 -0600

    Initial commit: Eleventy + Decap CMS setup for Netlify

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,15 @@
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy({ "src/images": "images" });
   eleventyConfig.addPassthroughCopy({ "src/admin": "admin" });
+  
+  // Collections
+  eleventyConfig.addCollection("artworks", function(collectionApi) {
+    return collectionApi.getFilteredByGlob("src/artworks/*.md");
+  });
+  
+  eleventyConfig.addCollection("posts", function(collectionApi) {
+    return collectionApi.getFilteredByGlob("src/posts/*.md");
+  });
+  
   return { dir: { input: "src", output: "_site" } };
 };
